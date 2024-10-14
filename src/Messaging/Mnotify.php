5948c8cf193e462faa5f6335ae1a2647ffeb6415
Sulemana Abdul Haafiz Bundana <bundanaabdulhafiz@gmail.com>
2024-10-14 11:07:57 +0000

    Update Mnotify.php

--- src/Messaging/Mnotify.php
+++ src/Messaging/Mnotify.php
@@ -1,8 +1,7 @@
-<?php
-
 namespace Bundana\Services\Messaging;
 
 use Bundana\Services\Requests\SMSHttps;
+use Illuminate\Support\Facades\Log;
 
 /**
  * This class represents the Mnotify messaging service.
@@ -16,6 +15,7 @@ class Mnotify extends Validator implements SMSProviderInterface
     private $api_key = null;
     private $sender_key = null;
     private $schedule_date = null;
+    private $version = 'v1'; // Default to v1
     private $query_params;
     private $newV2Keys = null;
 
@@ -38,7 +38,7 @@ class Mnotify extends Validator implements SMSProviderInterface
     /**
      * Sets the message for the Mnotify instance.
      *
-     * @param string $message The message to be set.
+     * @param string $message The message to be sent.
      * @return $this The Mnotify instance.
      */
     public function message($message)
@@ -47,137 +47,150 @@ class Mnotify extends Validator implements SMSProviderInterface
         return $this;
     }
 
-
-    public function newKeys($newKeys)
+    /**
+     * Sets API version (v1 or v2).
+     *
+     * @param string $version The API version to use.
+     * @return $this The Mnotify instance.
+     */
+    public function setVersion($version)
     {
-        $this->mnotifyNewKeys($newKeys);
-        $this->newKeys = $newKeys;
+        $this->version = $version;
         return $this;
-        //
     }
 
     /**
-     * Sends an API request to Mnotify service to send a message.
+     * Set new API keys dynamically.
      *
-     * @return void
+     * @param array $newKeys The API keys to use.
+     * @return $this The Mnotify instance.
      */
-    public function smsApiRequest($version = 'v1')
+    public function newKeys($newKeys)
     {
-        if (isset($this->newKeys) && !isset($version) && $version != 'v2') {
-            $this->api_key = $this->newKeys['apiKey'];
-            $this->sender_key = $this->newKeys['sender_id'];
-        } else {
-            $this->checkMnotifyKeysfromEnvironment();
-            $this->api_key = env('MNOTIFY_API_KEY', '');
-            $this->sender_key = env('MNOTIFY_SENDER_ID', '');
-        }
-        // Build the API request URL
-        $query_params = [
-            'key' => $this->api_key,
-            'to' => $this->phone,
-            'msg' => $this->message,
-            'sender_id' => $this->sender_key,
-        ];
+        $this->newKeys = $newKeys;
+        return $this;
+    }
 
-        if ($version == 'v2') {
-            $this->checkMnotifyVersion2KeysfromEnvironment();
-            $this->api_key = env('MNOTIFY_API_KEY_V2', '');
-            $this->sender_key = env('MNOTIFY_SENDER_ID_V2', '');
-            // Build the API request URL
-            $query_params = [
-                'key' => $this->api_key,
-                'recipient' => [$this->phone],
-                'message' => $this->message,
-                'sender' => $this->sender_key,
-                'is_schedule' => false,
-                'schedule_date' => ''
-            ];
-        }
+    /**
+     * Sends the notification through the Mnotify API.
+     *
+     * @return bool|string The API response or error message.
+     */
+    public function send($scheduleDate = null)
+    {
+        $this->schedule_date = $scheduleDate;
+        return $this->smsApiRequest();
+    }
+
+    /**
+     * Send the SMS via Mnotify API.
+     *
+     * @return bool|string The API response or error message.
+     */
+    private function smsApiRequest()
+    {
+        $this->setApiKeys();
+        $this->buildQueryParams();
 
         $https = new SMSHttps();
-        $https = $https->MnotifySMSRequest($query_params, $version);
-        $response = json_decode($https, true);
+        $response = $https->MnotifySMSRequest($this->query_params, $this->version);
+        $response = json_decode($response, true);
 
         if (isset($response['status']) && $response['status'] != 'success') {
+            Log::error("Mnotify Error: " . json_encode($response));
             return json_encode(['success' => false, 'message' => SMSHttps::MnotifySMSErrorMessage($response['code'])]);
         }
 
         return json_encode(['success' => true, 'message' => SMSHttps::MnotifySMSErrorMessage($response['code']), 'code' => $response['code']]);
-
     }
 
     /**
-     * Sends the notification.
-     *
-     * @return bool Returns true if the notification is successfully sent, false otherwise.
+     * Build the query parameters for the API request.
      */
-    public function send($version = 'v1', $date = null)
+    private function buildQueryParams()
     {
-        $this->schedule_date = $date;
-        // For now, return true as a placeholder for a successful send
-        return $this->smsApiRequest($version);
+        $this->query_params = [
+            'key' => $this->api_key,
+            'recipient' => [$this->phone],
+            'message' => $this->message,
+            'sender' => $this->sender_key,
+            'is_schedule' => !is_null($this->schedule_date),
+            'schedule_date' => $this->schedule_date ?? '',
+        ];
     }
 
     /**
-     * Sends bulk SMS messages to multiple contacts.
+     * Set API keys depending on version and environment.
+     */
+    private function setApiKeys()
+    {
+        if ($this->version === 'v2') {
+            $this->api_key = env('MNOTIFY_API_KEY_V2', $this->newKeys['apiKey'] ?? '');
+            $this->sender_key = env('MNOTIFY_SENDER_ID_V2', $this->newKeys['sender_id'] ?? '');
+        } else {
+            $this->api_key = env('MNOTIFY_API_KEY', $this->newKeys['apiKey'] ?? '');
+            $this->sender_key = env('MNOTIFY_SENDER_ID', $this->newKeys['sender_id'] ?? '');
+        }
+    }
+
+    /**
+     * Send bulk SMS to multiple contacts.
      *
-     * @param array $contactsAndMessages An associative array where the keys are the contacts and the values are the messages.
-     * @return array An associative array where the keys are the contacts and the values are the responses from sending the messages.
+     * @param array $contactsAndMessages An array of contacts and their respective messages.
+     * @return array The responses from sending the messages.
      */
     public static function sendBulk($contactsAndMessages, $version = 'v1')
     {
         $instance = new static();
-        $instance->mnotifyBulkSMSListValidator($contactsAndMessages);
+        $instance->setVersion($version);
         $responses = [];
 
         foreach ($contactsAndMessages as $contact => $message) {
-            $responses[$contact] = $instance->to($contact)->message($message)->send($version);
+            $responses[$contact] = $instance->to($contact)->message($message)->send();
         }
+
         return $responses;
     }
 
-
     /**
-     * Register Sender ID
+     * Register a Sender ID.
      *
-     * @param string $id The sender ID to be registered.
-     * @param string $purpose The purpose for registering the sender ID.
-     * @return string The response from the API request.
+     * @param string $id The sender ID to register.
+     * @param string $purpose The purpose of the registration.
+     * @return string The API response.
      */
     public static function registerSenderID($id, $purpose)
     {
         $https = new SMSHttps();
         $response = $https->MnotifyReqisterSenderID($id, $purpose);
         $response = json_decode($response, true);
+
         if (isset($response['status']) && $response['status'] !== 'success') {
+            Log::error("Mnotify Sender ID Error: " . json_encode($response));
             return json_encode(['success' => false, 'message' => $response['message']]);
         }
 
         return json_encode(['success' => true, 'message' => $response['message'], 'data' => $response['summary']]);
-
     }
 
     /**
-     * Checks the SMS balance for the Mnotify service.
+     * Check SMS balance for the Mnotify service.
      *
-     * @param string|null $version The version of the API to use. If set to 'v2', it uses version 2, otherwise it uses version 1.
-     * @return string The JSON-encoded response containing the success status and the SMS balance or error message.
+     * @param string|null $version API version to use.
+     * @return string The response including SMS balance or error message.
      */
     public static function SMSBalance($version = null)
     {
         $https = new SMSHttps();
-        $https = $https->MnotifySMSBalanceRequest($version);
-        $response = json_decode($https, true);
+        $response = json_decode($https->MnotifySMSBalanceRequest($version), true);
 
         if (isset($response['status']) && $response['status'] !== 'success') {
+            Log::error("Mnotify Balance Error: " . json_encode($response));
             return json_encode(['success' => false, 'message' => $response['message']]);
         }
 
-        if (isset($response['status']) && $response['status'] == 'success' && isset($response['sms_balance'])) {
-            return json_encode(['success' => true, 'balance' => $response['sms_balance']]);
-        } elseif (isset($response['status']) && $response['status'] == 'success' && isset($response['balance'])) {
-            return json_encode(['success' => true, 'balance' => $response['balance'], 'bonus' => $response['bonus']]);
-        }
-
+        return isset($response['sms_balance']) 
+            ? json_encode(['success' => true, 'balance' => $response['sms_balance']])
+            : json_encode(['success' => true, 'balance' => $response['balance'], 'bonus' => $response['bonus']]);
     }
 }
